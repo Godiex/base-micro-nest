5913d29a0da1679697a9cdf9cf779677699c94dd
dvillcar <diego.villa@devzeros.com>
2023-09-09 20:39:11 -0500

    ajuste paquete y config de ambientes

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,12 +1,19 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { NodeEnv } from './config/environment/env-node.enum';
 
 @Module({
   providers: [],
   imports: [
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `env/${process.env.NODE_ENV}.env`,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid(NodeEnv.DEVELOPMENT, NodeEnv.PRODUCTION)
+          .required(),
+      }),
     }),
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { EnvVariables } from './infrastructure/config/environment/env-variables.enum';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Bloque Arquitectura Hexagonal Node')
+    .setDescription(
+      "Bloque que hace uso de Nest.js para la creación de API's con Node.js",
+    )
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('/api/doc', app, swaggerDocument);
+
   await app.listen(configService.get(EnvVariables.APPLICATION_PORT));
+  app.setGlobalPrefix(configService.get(EnvVariables.APPLICATION_CONTEXT_PATH));
 }
 bootstrap();
